6d1f15e5922b5ef5dcdc10ab93694db901d58c33
zhcngu <zhcngu@163.com>
2020-05-24 20:18:04 +0800

    第一次提交,不知道会提交到哪个分支

---
+++ 通过libnodave获取PLC数据/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace 通过libnodave获取PLC数据
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.txtRead = new System.Windows.Forms.TextBox();
+            this.txtWrite = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "链接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(82, 112);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "读取";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(82, 186);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 2;
+            this.btnWrite.Text = "写入";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // txtRead
+            // 
+            this.txtRead.Location = new System.Drawing.Point(174, 114);
+            this.txtRead.Name = "txtRead";
+            this.txtRead.Size = new System.Drawing.Size(326, 21);
+            this.txtRead.TabIndex = 3;
+            // 
+            // txtWrite
+            // 
+            this.txtWrite.Location = new System.Drawing.Point(174, 188);
+            this.txtWrite.Name = "txtWrite";
+            this.txtWrite.Size = new System.Drawing.Size(326, 21);
+            this.txtWrite.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 328);
+            this.Controls.Add(this.txtWrite);
+            this.Controls.Add(this.txtRead);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.TextBox txtRead;
+        private System.Windows.Forms.TextBox txtWrite;
+    }
+}
+
---
+++ 通过libnodave获取PLC数据/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 通过libnodave获取PLC数据
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        libnodave.daveConnection dc;
+        libnodave.daveInterface di;
+        libnodave.daveOSserialType fds;
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+           fds.rfd = libnodave.openSocket(102, "192.168.186.130");
+           fds.wfd = fds.rfd;
+            if (fds.rfd > 0)
+            {
+                di = new libnodave.daveInterface(fds, "IF1", 2,libnodave.daveProtoISOTCP, libnodave.daveSpeed1500k);//libnodave.daveProtoISOTCP
+                di.setTimeout(0xbb8);
+                dc = new libnodave.daveConnection(di, 0, 2, 0);
+                 int  res=dc.connectPLC();
+                if (res == 0)
+                {
+                    MessageBox.Show("链接到PLC");
+                }
+            }
+        }
+
+       
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            byte[] buff = new byte[8];
+            //byte[] buff2 = new byte[3000];
+            int x1 = this.dc.readBits(libnodave.daveDB, 100, 110 * 8+1, 1, buff);//  DB100.110.1这个点
+            //int x2 = this.dc.readManyBytes(libnodave.daveDB, 100, 344, 20, buff2);//
+
+
+            //byte[] buff3 = new byte[1];   //0,1.0,0
+            //int res = dc.readBits(libnodave.daveFlags, 100, 124*8+5, 1, buff3);
+
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            byte[] buff = { 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48 };
+            int res=dc.writeBytes(libnodave.daveDB,1450, 64, 11, buff);
+            MessageBox.Show(res.ToString());
+          
+          
+        }
+    }
+}
